d6d20dc2608f80b4c32e563b00a1cf090f62b984
curly72 <curly72@26c7a42c-a214-0410-8a99-c1a46ea20dee>
2012-05-08 18:34:50 +0000

    updated license plugin config

--- run.bat
+++ run.bat
@@ -1,1 +1,22 @@
-mvn exec:java -Dexec.mainClass="org.synchronoss.utils.cpo.CpoUtil" -e
+@REM
+@REM Copyright (C) 2003-2012 David E. Berry, Michael A. Bellomo
+@REM
+@REM This library is free software; you can redistribute it and/or
+@REM modify it under the terms of the GNU Lesser General Public
+@REM License as published by the Free Software Foundation; either
+@REM version 2.1 of the License, or (at your option) any later version.
+@REM
+@REM This library is distributed in the hope that it will be useful,
+@REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+@REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+@REM Lesser General Public License for more details.
+@REM
+@REM You should have received a copy of the GNU Lesser General Public
+@REM License along with this library; if not, write to the Free Software
+@REM Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+@REM
+@REM A copy of the GNU Lesser General Public License may also be found at
+@REM http://www.gnu.org/licenses/lgpl.txt
+@REM
+
+mvn exec:java -Dexec.mainClass="org.synchronoss.cpo.util.CpoUtil" -e
